8e188b374c5a536282c585e989de16dd50f592ab
jujubean22 <jmevillanueva22@gmail.com>
2022-03-31 14:09:36 +0800

    added initial pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import './App.css'
-import {db} from './firebase'
-import { uid } from "uid";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import About from "./pages/About";
+import AddEdit from "./pages/AddEdit";
+import Home from "./pages/Home";
+import View from "./pages/View";
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
-    <div className="App">
-        <p>
-          Menu
-        </p>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <ToastContainer position="top-center"/>
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route path="/add" element={<AddEdit/>} />
+          <Route path="/update/:id" element={<AddEdit/>} />
+          <Route path="/view/:id" element={<View/>} />
+          <Route path="/about" element={<About/>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,4 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
@@ -12,6 +11,5 @@ const firebaseConfig = {
   measurementId: "G-NLD56CT5EV"
 };
 
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-export const db = getDatabase(app)
+const fireDb = initializeApp(firebaseConfig);
+export const db = getDatabase(fireDb).ref()
